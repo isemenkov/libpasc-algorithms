cd85fcc485fed51299fb28deb6482cd325c90e7f
iXakeR <ivan@semenkov.pro>
2020-07-02 20:28:30 +0300

    Update TList

--- source/list.pas
+++ source/list.pas
@@ -37,12 +37,54 @@ uses
   SysUtils;
 
 type
+  { List item value not exists. }
+  EValueNotExistsException = class(Exception);
+
   { Doubly-linked list.
     A doubly-linked list stores a collection of values. Each entry in the list 
     (represented by a pointer a @ref ListEntry structure) contains a link to the 
     next entry and the previous entry. It is therefore possible to iterate over 
     entries in the list in either direction. }
   generic TList<T> = class
+  protected
+    type
+      { TList item entry type. }
+      PListEntry = ^TListEntry;
+      TListEntry = record
+        Value : T;
+        Prev : PListEntry;
+        Next : PListEntry; 
+      end;
+  public
+    type
+      { TList iterator. }
+      TIterator = class
+      public
+        { Create new iterator for list item entry. }
+        constructor Create (AItem : PListEntry);
+
+        { Retrieve the previous entry in a list. }
+        function Prev : TIterator;
+
+        { Retrieve the next entry in a list. }
+        function Next : TIterator;
+
+        { Remove an entry from a list. }
+        procedure Remove;
+      protected
+        { Get item value. }
+        function GetValue : T;
+
+        { Set new item value. }
+        procedure SetValue (AValue : T);
+      protected
+        var
+          FItem : PListEntry;
+      public
+        { Read/Write list item value. If value not exists raise 
+          EValueNotExistsException. }
+        property Value : T read GetValue write SetValue;
+      end;
   public
     { Free an entire list. }
     destructor Destroy; override;
@@ -57,21 +99,32 @@ type
       allocate more memory for the new entry. }
     function Append (AData : T) : Boolean;
 
-    { Retrieve the next entry in a list. 
-      The next entry in the list, or nil if this was the last entry in the 
-      list. }
-    function Next : T;
+    { Remove all occurrences of a particular value from a list. Return the 
+      number of entries removed from the list. }
+    function Remove (AData : T) : Cardinal;
+
+    { Retrive the first entry in a list. }
+    function FirstEntry : TIterator;
+
+    { Retrive the last entry in a list. }
+    function LastEntry : TIterator;
+
+    { Retrieve the entry at a specified index in a list. }
+    function NthEntry (AIndex : Cardinal) : TIterator;
+
+    { Find the entry for a particular value in a list. }
+    function FindEntry (AData : T) : TIterator;
+
+    { Sort a list. }
+    procedure Sort;
   protected
-    type
-      PListEntry = ^TListEntry;
-      TListEntry = record
-        Value : T;
-        Prev : PListEntry;
-        Next : PListEntry; 
-      end;
     var
-      FHead : PListEntry;
-      FCurrent : PListEntry;       
+      FFirstNode : PListEntry;
+      FLastNode : PListEntry;     
+      FLength : Cardinal;
+  public
+    { Get List length. }
+    property Length : Cardinal read FLength;  
   end;
 
 implementation
