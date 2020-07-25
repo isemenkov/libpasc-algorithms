f5fd3fc6e5f133059197e7e3b835cb50fca5ee62
Ivan Semenkov <ivan@semenkov.pro>
2020-07-25 12:12:13 +0300

    Update unit-test Update README

--- README.md
+++ README.md
@@ -25,3 +25,17 @@ type
 ```
 
 A doubly-linked list stores a collection of values. Each entry in the list contains a link to the next entry and the previous entry. It is therefore possible to iterate over entries in the list in either direction.
+
+##### TAvlTree
+
+```pascal
+uses
+  avl_tree;
+ 
+type
+  generic TAvlTree<K, V> = class
+```
+
+The AVL tree structure is a balanced binary tree which stores a collection of nodes. Each node has a key and a value associated with it. The nodes are sorted within the tree based on the order of their keys. Modifications to the tree are constructed such that the tree remains balanced at all times (there are always roughly equal numbers of nodes on either side of the tree).
+
+Balanced binary trees have several uses. They can be used as a mapping (searching for a value based on its key), or as a set of keys which is always ordered.
--- unit-tests/avltreetestcase.pas
+++ unit-tests/avltreetestcase.pas
@@ -14,8 +14,9 @@ type
   TAvlTreeTestCase = class(TTestCase)
   published
     procedure TestIntIntTreeInsert;
-    procedure TestStringIntTreeInsert;
     procedure TestIntIntTreeRemove;
+    procedure TestStringIntTreeInsert;
+    procedure TestStringIntTreeRemove;
   end;
 
 implementation
@@ -60,6 +61,7 @@ begin
   AssertTrue('Tree value 1 is not removed', tree.Remove(1));
   AssertTrue('Tree value 2 is not removed', tree.Remove(2));
   AssertTrue('Tree value 5 is not removed', tree.Remove(5));
+  AssertTrue('Tree not exists value is removed', not tree.Remove(7));
 
   FreeAndNil(tree);
 end;
@@ -83,6 +85,28 @@ begin
   AssertTrue('Tree value test10 is not correct', tree.Search('test10') = 10);
   AssertTrue('Tree value test12 is not correct', tree.Search('test12') = 12);
   AssertTrue('Tree value test132 is not correct', tree.Search('test132') = 132);
+
+  FreeAndNil(tree);
+end;
+
+procedure TAvlTreeTestCase.TestStringIntTreeRemove;
+var
+  tree : TStringIntTree;
+begin
+  tree := TStringIntTree.Create;
+
+  tree.Insert('test1', 1);
+  tree.Insert('test2', 2);
+  tree.Insert('test4', 4);
+
+  AssertTrue('Tree value test1 is not correct', tree.Search('test1') = 1);
+  AssertTrue('Tree value test2 is not correct', tree.Search('test2') = 2);
+  AssertTrue('Tree value test4 is not correct', tree.Search('test4') = 4);
+
+  AssertTrue('Tree value test1 is not removed', tree.Remove('test1'));
+  AssertTrue('Tree value test2 is not removed', tree.Remove('test2'));
+  AssertTrue('Tree value test4 is not removed', tree.Remove('test4'));
+  AssertTrue('Tree not exists value is removed', not tree.Remove('test5'));
 
   FreeAndNil(tree);
 end;
