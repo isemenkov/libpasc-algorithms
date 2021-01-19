19703de3501bab905626d06864e0918c3d45655a
Ivan Semenkov <ivan@semenkov.pro>
2021-01-19 11:10:15 +0200

    Update TRedBlackTree

--- source/container.redblacktree.pas
+++ source/container.redblacktree.pas
@@ -3,7 +3,7 @@
 (* delphi and object pascal library of  common data structures and algorithms *)
 (*                 https://github.com/fragglet/c-algorithms                   *)
 (*                                                                            *)
-(* Copyright (c) 2020                                       Ivan Semenkov     *)
+(* Copyright (c) 2021                                       Ivan Semenkov     *)
 (* https://github.com/isemenkov/libpasc-algorithms          ivan@semenkov.pro *)
 (*                                                          Ukraine           *)
 (******************************************************************************)
@@ -94,7 +94,8 @@ type
       TOptionalValue = {$IFDEF FPC}specialize{$ENDIF} TOptional<V>;
       {$ENDIF} 
   public
-
+    constructor Create;
+    destructor Destroy; override;
   protected
     function TreeNodeSide (node : PRedBlackNode) : TRedBlackNodeSide;
 
@@ -125,6 +126,50 @@ type
         A   C                           C   E                                  }
     function TreeRotate (node : PRedBlackNode; direction : TRedBlackNodeSide) :
       PRedBlackNode;
+
+    procedure TreeFreeSubTree (node : PRedBlackNode);
+
+    { Insert case 1: If the new node is at the root of the tree, it must be 
+      recolored black, as the root is always black. }
+    procedure TreeInsertCase1 (node : PRedBlackNode);
+
+    { Insert case 2: If the parent of the new node is red, this conflicts with 
+      the red-black tree conditions, as both children of every red node are 
+      black. }
+    procedure TreeInsertCase2 (node : PRedBlackNode);
+    
+    { Insert case 3: If the parent and uncle are both red, repaint them both 
+      black and repaint the grandparent red. }
+    procedure TreeInsertCase3 (node : PRedBlackNode);
+
+    { Case 4: If the parent is red, but the uncle is black, we need to do some 
+      rotations to keep the tree balanced and complying with the tree 
+      conditions.  If the node is on the opposite side relative to its parent
+      as the parent is relative to its grandparent, rotate around the parent.  
+      Either way, we will continue to case 5.
+ 
+      eg.
+ 
+           B                              B
+          / \                            / \
+         R   B          ->     node ->  R   B
+          \                            /
+           R  <- node                 R                                        }
+    procedure TreeInsertCase4 (node : PRedBlackNode);
+
+    { Case 5: The node is on the same side relative to its parent as the parent 
+      is relative to its grandparent. The node and its parent are red, but the 
+      uncle is black.
+ 
+      Now, rotate at the grandparent and recolor the parent and grandparent to 
+      black and red respectively.
+ 
+                 G/B                 P/B
+                /   \               /   \
+             P/R     U/B    ->   N/R     G/R
+            /   \                       /   \
+         N/R      ?                   ?      U/B                               }
+    procedure TreeInsertCase5 (node : PRedBlackNode);
   protected
     FTree : PRedBlackTreeStruct;
     FCompareFunctor : KeyBinaryCompareFunctor;
@@ -133,6 +178,25 @@ type
 implementation
 
 { TReadBlackTree }
+
+constructor TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
+  .Create;
+begin
+  New(FTree);
+  FTree^.root_node := nil;
+  FTree^.num_nodes := 0;
+  FCompareFunctor := KeyBinaryCompareFunctor.Create;
+end;
+
+destructor TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
+  .Destroy;
+begin
+  { Free all nodes in the tree. }
+  TreeFreeSubTree(FTree^.root_node);
+
+  { Free back the main tree structure. }
+  Dispose(FTree);
+end;
 
 function TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
   .TreeNodeSide (node : PRedBlackNode) : TRedBlackNodeSide;
@@ -202,6 +266,116 @@ begin
     node^.children[1 - direction]^.parent := node;
 
   Result := new_root;
+end;
+
+procedure TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
+  .TreeFreeSubTree (node : PRedBlackNode);
+begin
+  if node <> nil then
+  begin
+    { Recurse to subnodes. }
+    TreeFreeSubTree(node^.children[RB_TREE_NODE_LEFT]);
+    TreeFreeSubTree(node^.children[RB_TREE_NODE_RIGHT]);
+
+    { Free this node. }
+    Dispose(node);
+  end;
+end;
+
+procedure TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
+  .TreeInsertCase1 (node : PRedBlackNode);
+begin
+  if node^.parent = nil then
+  begin
+    { The root node is black. }
+    node^.color := RB_TREE_NODE_BLACK;
+  end else
+  begin
+    { Not root. }
+    TreeInsertCase2(node);
+  end;
+end;
+
+procedure TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
+  .TreeInsertCase2 (node : PRedBlackNode);
+begin
+  { Note that if this function is being called, we already know the node has a 
+    parent, as it is not the root node. }
+  if node^.parent^.color <> RB_TREE_NODE_BLACK then
+    TreeInsertCase3(node);
+end;
+
+procedure TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
+  .TreeInsertCase3 (node : PRedBlackNode);
+var
+  grandparent, uncle : PRedBlackNode;
+begin
+  { Note that the node must have a grandparent, as the parent is red, and the 
+    root node is always black. }
+  grandparent := node^.parent^.parent;
+  uncle := TreeNodeUncle(node);
+
+  if (uncle <> nil) and (uncle^.color = RB_TREE_NODE_RED) then
+  begin
+    node^.parent^.color := RB_TREE_NODE_BLACK;
+    uncle^.color := RB_TREE_NODE_BLACK;
+    grandparent^.color := RB_TREE_NODE_RED;
+
+    { Recurse to grandparent. }
+    TreeInsertCase1(grandparent);
+  end else
+  begin
+    TreeInsertCase4(node);
+  end;
+end;
+
+procedure TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
+  .TreeInsertCase4 (node : PRedBlackNode);
+var
+  next_node : PRedBlackNode;
+  side : TRedBlackNodeSide;
+begin
+  { Note that at this point, by implication from case 3, we know that the parent 
+    is red, but the uncle is black. We therefore only need to examine what side 
+    the node is on relative to its parent, and the side the parent is on 
+    relative to the grandparent. }
+  side := TreeNodeSide(node);
+
+  if side <> TreeNodeSide(node^.parent) then
+  begin
+    { After the rotation, we will continue to case 5, but the parent node will 
+      be at the bottom. }
+    next_node := node^.parent;
+
+    { Rotate around the parent in the opposite direction to side. }
+    TreeRotate(node^.parent, 1 - side);
+  end else
+  begin
+    next_node := node;
+  end;
+
+  TreeInsertCase5(next_node);
+end;
+
+procedure TRedBlackTree{$IFNDEF FPC}<K, V, KeyBinaryCompareFunctor>{$ENDIF}
+  .TreeInsertCase5 (node : PRedBlackNode);
+var
+  parent, grandparent : PRedBlackNode;
+  side : TRedBlackNodeSide;
+begin
+  parent := node^.parent;
+  grandparent := parent^.parent;
+
+  { What side are we, relative to the parent? This will determine the direction 
+    that we rotate. }
+  side := TreeNodeSide(node);
+
+  { Rotate at the grandparent, in the opposite direction to side. }
+  TreeRotate(grandparent, 1 - side);
+
+  { Recolor the (old) parent and grandparent. }
+  parent^.color := RB_TREE_NODE_BLACK;
+  grandparent^.color := RB_TREE_NODE_RED;
 end;
 
 end.
